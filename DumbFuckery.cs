f7075b65e7c77c9790e1cb074fba73f25e43b099
DigitalzombieTLD <christian.gendner@gmx.net>
2021-11-28 01:04:29 +0100

    Add project files.

---
+++ DumbFuckery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MelonLoader;
+using Harmony;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System.Xml.XPath;
+using System.Globalization;
+using UnhollowerRuntimeLib;
+using AMD_FIDELITY_FX;
+
+namespace FidelityFX
+{
+	public class DumbFuckery_Main : MonoBehaviour
+	{
+		public static void DoSomeShizz()
+		{			
+			if(FidelityFX_Main.FidelityFXBundle != null)
+			{
+		
+			}
+		}
+
+		public static void DoShizzAllTheTime()
+		{			
+			if (InputManager.GetKeyDown(InputManager.m_CurrentContext, KeyCode.Keypad0))
+			{
+				MelonLogger.Msg("*Doing shizz!!!*");
+				GameManager.m_MainCamera.gameObject.AddComponent<AMD_FIDELITY_FX_FSR>();
+				
+				DoSomeShizz();
+			}
+		}
+	}
+}
---
+++ FidelityFX.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MelonLoader;
+using Harmony;
+using UnityEngine;
+using System.Reflection;
+using System.Xml.XPath;
+using System.Globalization;
+using UnhollowerRuntimeLib;
+using AMD_FIDELITY_FX;
+
+namespace FidelityFX
+{
+    public class FidelityFX_Main : MelonMod
+    {
+		public static AssetBundle FidelityFXBundle;
+		public static ComputeShader EdgeAdaptiveScaleUpsampling;
+		public static ComputeShader RobustContrastAdaptiveSharpen;
+
+		public static bool followPlayer = false;
+
+		public override void OnApplicationStart()
+        {
+			ClassInjector.RegisterTypeInIl2Cpp<AMD_FIDELITY_FX_FSR>();
+
+			FidelityFXBundle = AssetBundle.LoadFromFile("Mods\\ffx.unity3d");
+
+			if (FidelityFXBundle == null)
+			{
+				MelonLogger.Msg("Failed to load AssetBundle (ffx.unity3d)!");
+				return;
+			}
+
+			EdgeAdaptiveScaleUpsampling = FidelityFXBundle.LoadAsset<ComputeShader>("EdgeAdaptiveScaleUpsampling");
+			RobustContrastAdaptiveSharpen = FidelityFXBundle.LoadAsset<ComputeShader>("RobustContrastAdaptiveSharpen");
+
+			MelonLogger.Msg("ffx.unity3d succesfully loaded");
+			
+		}
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			
+		}
+
+		public override void OnUpdate()
+		{
+			DumbFuckery_Main.DoShizzAllTheTime();
+		}
+	}
+}
